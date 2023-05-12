f57794b60ca4737f91c03ef29eb411abf0621a97
Jeniffer Souza Ribeiro <jesouza774@gmail.com>
2023-05-12 15:25:17 -0300

    react01

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Home from './paginas/home/Home'
+
+ReactDOM.render(
+
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root') as HTMLElement
+);
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+function Home(){
+    return(
+    <>
+    
+            <h1 className='titulo' >React Funcionando !!</h1>
+            <img className='img' src="https://api.memegen.link/images/rollsafe/When_you_have_a_really_good_idea.gif?layout=top&width=800&token=orgyyu0tuzir7n4ktwvc" alt="" />
+    </>
+    )
+}
+
+export default Home;
